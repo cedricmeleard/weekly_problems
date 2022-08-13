6bbb073c85af2f5904482c10779f1bd55d115089
Cédric Méléard <cedric.meleard@gmail.com>
2022-08-13 08:45:56 +0200

    Swap Paids - rename methods & and comments

--- dotnet/swapPairs/SwapPairs/SwapPairs.cs
+++ dotnet/swapPairs/SwapPairs/SwapPairs.cs
@@ -2,36 +2,37 @@
 {
     public class SwapPairs
     {
-        public int[] Convert(int[] entry)
+        public int[] DoSwap(int[] entry)
         {
-            if (entry == null) return new int[]{ };
+            if (entry == null) return new int[] { };
 
             var result = entry.Clone() as int[];
             for (var i = 0; i <= result.Length - 2; i += 2)
             {
                 var temp = (int)result[i];
-                result[i] = result[i+1];
-                result[i+1] = temp;
+                result[i] = result[i + 1];
+                result[i + 1] = temp;
             }
-            
+
             return result;
         }
 
-        public LinkedList<int> Convert(LinkedList<int> entry)
+        public LinkedList<int> DoSwap(LinkedList<int> entry)
         {
             if (entry == null) return new LinkedList<int>();
 
             var result = new LinkedList<int>();
+            // we copy list to be sure we don't modify original list and introduce side effects
             var copy = new LinkedList<int>(entry);
 
-            while(copy.Count > 0)
+            while (copy.Count > 0)
             {
+                // get the first two elements
                 var toRight = Pop(copy);
                 var toLeft = Pop(copy);
-
+                // invert if exists (at this point toLeft coult be null)
                 if (toLeft != null)
                     result.AddLast(toLeft.Value);
-                
                 if (toRight != null)
                     result.AddLast(toRight.Value);
             }
@@ -41,15 +42,15 @@
         /// <summary>
         /// Remove first element of a list and return it
         /// </summary>
-        /// <param name="copy"></param>
+        /// <param name="stacklist"></param>
         /// <remarks>TODO should be change to extenstion of linkedlist</remarks>
         /// <returns></returns>
-        private static int? Pop(LinkedList<int> copy)
+        private static int? Pop(LinkedList<int> stacklist)
         {
-            var firstElement = copy.First;
+            var firstElement = stacklist.First;
             if (firstElement == null) return null;
 
-            copy.RemoveFirst();
+            stacklist.RemoveFirst();
             return firstElement.Value;
         }
     }
