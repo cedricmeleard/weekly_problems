9e379e05f783e9f28e37136f9e73518454add4eb
Cédric Méléard <cedric.meleard@gmail.com>
2022-09-19 20:13:11 +0200

    starting next hit version, not working yet

--- js/bouncinglogo/boucinglogo.js
+++ js/bouncinglogo/boucinglogo.js
@@ -24,4 +24,61 @@ const checkIsOnLineToCorner = (from, to) => {
   return to[0] - from[0] === to[1] - from[1];
 };
 
-export { cornerHit };
+const direction = {
+  SOUTHEAST: [1, 1],
+  NORTHWEST: [-1, -1],
+  SOUTHWEST: [-1, 1],
+  NORTHEAST: [1, -1]
+};
+class BouncingLogo {
+  constructor() {
+    this.screenSize = [100, 100];
+    this.dimensions = [5, 5];
+    // top left coordinates
+    this.initialCoordinates = [0, 0];
+    this.direction = direction.SOUTHEAST;
+  }
+
+  withScreenSize(screenSize) {
+    this.screenSize = screenSize;
+    return this;
+  }
+  withStartPosition(initialCoordinates) {
+    this.initialCoordinates = initialCoordinates;
+    return this;
+  }
+  withDimensions(dimensions) {
+    this.dimensions = dimensions;
+    return this;
+  }
+
+  getNumberOfMove() {
+    let count = 1;
+    // this can lead to infinite loop, therefore we limit to 100 bouncing round
+    while (!this.checkIsOnLineToCorner() && count < 100) {
+      count++;
+    }
+    return count;
+  }
+
+  getAngleToTestCollision() {
+    switch (this.direction) {
+      case direction.SOUTHEAST:
+        return [this.initialCoordinates[0] + this.dimensions[0], this.initialCoordinates[1] + this.dimensions[1]];
+      default:
+        throw Error("getAngleToTestCollision - pas bon");
+    }
+  }
+
+  checkIsOnLineToCorner() {
+    var actualPosition = this.getAngleToTestCollision();
+    switch (this.direction) {
+      case direction.SOUTHEAST:
+        return this.screenSize[0] - actualPosition[0] === this.screenSize[1] - actualPosition[1];
+      default:
+        throw Error("checkIsOnLineToCorner - pas bon");
+    }
+  }
+}
+
+export { cornerHit, BouncingLogo };
--- js/bouncinglogo/boucinglogo.spec.js
+++ js/bouncinglogo/boucinglogo.spec.js
@@ -1,6 +1,6 @@
-import { cornerHit } from "./boucinglogo";
+import { cornerHit, BouncingLogo } from "./boucinglogo";
 
-describe("testing boucing logo", () => {
+describe("testing cornerHit", () => {
   test("should hit the corner", () => {
     let dimensions = [5, 5];
     let initialCoordinates = [0, 0]; // you decide which part of the logo the coords map to
@@ -12,5 +12,19 @@ describe("testing boucing logo", () => {
   test("should not hit the corner", () => {
     let dimensions = [5, 5];
     expect(cornerHit(dimensions, [45, 70], [400, 200])).toBeFalsy();
+  });
+});
+
+describe("testing cornerHit", () => {
+  test("should hit the corner", () => {
+    const sut = new BouncingLogo().withDimensions([5, 5]).withStartPosition([0, 0]).withScreenSize([100, 100]);
+
+    expect(sut.getNumberOfMove()).toEqual(1);
+  });
+
+  test("should not hit the corner", () => {
+    const sut = new BouncingLogo().withDimensions([5, 5]).withStartPosition([45, 70]).withScreenSize([400, 200]);
+
+    expect(sut.getNumberOfMove()).toEqual(100);
   });
 });
